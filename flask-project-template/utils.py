9180ca37d82df692148c3f52a57498a563cb4ecf
diegotony <tucotony1396@gmail.com>
2020-03-28 16:19:11 -0500

    cration function created

--- flask-project-template/utils.py
+++ flask-project-template/utils.py
@@ -3,10 +3,14 @@ import errno
 import os
 
 with open('yamls/init.yml','r') as f:
-    init = yaml.load(f,Loader=yaml.FullLoader)
+    init_file = yaml.load(f,Loader=yaml.FullLoader)
 
 with open('yamls/app.yml','r') as f:
-    app = yaml.load(f,Loader=yaml.FullLoader)
+    app_file = yaml.load(f,Loader=yaml.FullLoader)
+
+
+with open('yamls/main.yml','r') as f:
+    main_file = yaml.load(f,Loader=yaml.FullLoader)
 
 # def create_app(name):
 #     try:
@@ -19,15 +23,22 @@ with open('yamls/app.yml','r') as f:
 
 def create_project(name):
     create_dir(name)
-    create_directories(name,init['directory'])
-    create_files(name,init['files'])
+    create_directories(name,init_file['directory'])
+    create_files(name,init_file['files'])
     
-    for i in init['directory']:
+    for i in init_file['directory']:
         if i == "app":
-            for i in app['directory']:
-                print(i)
-            # create_directories(name+"/"+i,app['directory'])
-            # create_files(name+"/"+i,app['files'])
+            for j in app_file['directory']:
+                print(j)
+                create_directories(name+"/"+"app",app_file['directory'])
+                create_files(name+"/"+"app",app_file['files'])
+                if j == "main":
+                    for k,v in main_file['directory'].items():
+                        path = name+"/"+"app"+"/"+"main"+"/"
+                        create_dir(path+k)
+                        for fi in v:
+                            create_file(path+k+"/"+fi)
+
 
         
 
@@ -38,7 +49,10 @@ def create_directories(path,directories):
 
 def create_files(name,files):
     for i in files:
-        open(name+"/"+i,'w+')
+        create_file(name+"/"+i)
+
+def create_file(path):
+    open(path,'w+')
 
 def create_folder():
     pass
