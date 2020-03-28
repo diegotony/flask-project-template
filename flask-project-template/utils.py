693d5bc5503b3caf7594b198cc93391c01dd4adf
diegotony <tucotony1396@gmail.com>
2020-03-27 22:42:41 -0500

    init app

--- flask-project-template/utils.py
+++ flask-project-template/utils.py
@@ -2,23 +2,46 @@ import yaml
 import errno
 import os
 
-with open('data.yml','r') as f:
-    data = yaml.load(f,Loader=yaml.FullLoader)
+with open('ymals/init.yml','r') as f:
+    init = yaml.load(f,Loader=yaml.FullLoader)
 
+with open('ymals/app.yml','r') as f:
+    app = yaml.load(f,Loader=yaml.FullLoader)
 
-def get_directories(path,directories):
+# def create_app(name):
+#     try:
+#         create_dir(name)
+#         create_directories(name,yaml_data(data,"proyect_structure","init","directory"))
+#     except OSError as e :
+#         if e.errno != errno.EEXIST:
+#             raise
+#         pass
+
+def create_project(name):
+    create_dir(name)
+    create_directories(name,init['directory'])
+    create_files(name,init['files'])
+    
+    for i in init['directory']:
+        if i == "app":
+            for i in app['directory']:
+                print(i)
+            # create_directories(name+"/"+i,app['directory'])
+            # create_files(name+"/"+i,app['files'])
+
+        
+
+def create_directories(path,directories):
     for dir in directories:
-        create_dir(path+"/"+dir)
+        f= create_dir(path+"/"+dir)
 
 
-def create_app(name):
-    try:
-        create_dir(name)
-        get_directories(name,yaml_data(data,"proyect_structure","init","directory"))
-    except OSError as e :
-        if e.errno != errno.EEXIST:
-            raise
-        pass
+def create_files(name,files):
+    for i in files:
+        open(name+"/"+i,'w+')
+
+def create_folder():
+    pass
 
 
 def yaml_data(data,first,second,third):
@@ -27,7 +50,7 @@ def yaml_data(data,first,second,third):
 def create_dir(path):
     return os.makedirs(path,exist_ok=True)
 
-# TODO get_files
+
 # TODO create_dirs
 # TODO create_files
  
