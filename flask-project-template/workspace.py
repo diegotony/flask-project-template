905e97e5f369119c71980dbeeb4e97265a0369c4
Diego <tucotony1396@gmail.com>
2020-04-20 23:55:33 -0500

    clean file

--- flask-project-template/workspace.py
+++ flask-project-template/workspace.py
@@ -38,27 +38,31 @@ def check_str(test, path):
         create_file(path+"/"+test)
 
 
-def create_workspace(yaml_file):
+def create_workspace(yaml_file,name):
     for name_app, structure in yaml_file.items():
-        create_dir(name_app)
-        check_list(structure, name_app)
-        check_str(structure, name_app)
+        name_project = name
+        user  = os.getenv("USER")
+        print(user)
+        # os.chdir('/home/{0}/'.format(user))
+        create_dir(name_project)
+        check_list(structure, name_project)
+        check_str(structure, name_project)
         if check_dict(structure) is True:
             for folder, files in structure.items():
-                check_list(files, name_app)
-                check_str(files, name_app)
+                check_list(files, name_project)
+                check_str(files, name_project)
                 if check_dict(files) is True:
-                    create_dir(name_app+"/"+folder)
+                    create_dir(name_project+"/"+folder)
                     for d, r in files.items():
-                        create_dir(name_app+"/"+folder)
-                        check_list(r, name_app+"/"+folder)
-                        check_str(r, name_app+"/"+folder)
+                        create_dir(name_project+"/"+folder)
+                        check_list(r, name_project+"/"+folder)
+                        check_str(r, name_project+"/"+folder)
                         if check_dict(r) is True:
-                            create_dir(name_app+"/"+folder+"/"+d)
+                            create_dir(name_project+"/"+folder+"/"+d)
                             for q, w in r.items():
-                                create_dir(name_app+"/"+folder+"/"+d+"/"+q)
-                                check_list(w,name_app+"/"+folder+"/"+d+"/"+q)
-                                check_str(w,name_app+"/"+folder+"/"+d+"/"+q)
+                                create_dir(name_project+"/"+folder+"/"+d+"/"+q)
+                                check_list(w,name_project+"/"+folder+"/"+d+"/"+q)
+                                check_str(w,name_project+"/"+folder+"/"+d+"/"+q)
 
         # * create main directory
         # * check if is list or dict
