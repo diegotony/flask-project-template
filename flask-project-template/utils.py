d70f35512ce36777897b7a639e00d4759a1a8fa7
Diego <tucotony1396@gmail.com>
2020-04-20 23:54:08 -0500

    clean files

--- flask-project-template/utils.py
+++ flask-project-template/utils.py
@@ -17,35 +17,8 @@ with open('yamls/main.yml', 'r') as f:
 with open('yamls/project_template.yaml', 'r') as f:
     project = yaml.load(f, Loader=yaml.FullLoader)
 
-# def create_app(name):
-#     try:
-#         create_dir(name)
-#         create_directories(name,yaml_data(data,"proyect_structure","init","directory"))
-#     except OSError as e :
-#         if e.errno != errno.EEXIST:
-#             raise
-#         pass
-
-
 def create_project(name):
-    create_workspace(project)
-    # create_dir(name)
-    # create_directories(name, init_file['directory'])
-    # create_files(name, init_file['files'])
-
-    # for i in init_file['directory']:
-    #     if i == "app":
-    #         for j in app_file['directory']:
-    #             print(j)
-    #             create_directories(name+"/"+"app", app_file['directory'])
-    #             create_files(name+"/"+"app", app_file['files'])
-    #             if j == "main":
-    #                 for k, v in main_file['directory'].items():
-    #                     path = name+"/"+"app"+"/"+"main"+"/"
-    #                     create_dir(path+k)
-    #                     for fi in v:
-    #                         create_file(path+k+"/"+fi)
-
+    create_workspace(project, name)
 
 def yaml_data(data, first, second, third):
     return data[first][second][third]
