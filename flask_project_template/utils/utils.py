f8f55a45f9c8d32a579b5bf859275a8527189a0d
Diego <tucotony1396@gmail.com>
2020-04-22 22:39:46 -0500

    separate utils giles

--- flask_project_template/utils/utils.py
+++ flask_project_template/utils/utils.py
@@ -1,14 +1,47 @@
-import yaml
-
-from flask_project_template.workspace import create_workspace, create_directories, create_dir, create_files, create_file
-
-with open('flask_project_template/yamls/project_template.yaml', 'r') as f:
-    project = yaml.load(f, Loader=yaml.FullLoader)
+import os
+from flask_project_template.utils.jinja_utils import render_template
 
 
-def create_project(name):
-    create_workspace(project, name)
+def create_files(name, files):
+    for i in files:
+        create_file_jinja(name + "/" + i, i)
 
 
-def hello_world():
-    return "holi"
+def create_file(path):
+    open(path, 'w+')
+
+
+def create_file_jinja(path, name):
+    print(name)
+    with open(path, 'w+') as file:
+        file.write(render_template(name))
+
+
+def create_directories(path, directories):
+    for directory in directories:
+        create_dir(path + "/" + directory)
+
+
+def create_dir(path):
+    return os.makedirs(path, exist_ok=True)
+
+
+def check_dict(test):
+    if type(test) is dict:
+        return True
+    else:
+        return False
+
+
+def check_list(test, path):
+    if type(test) is list:
+        for i in test:
+            create_file_jinja(path + "/" + i, i)
+
+
+def check_str(test, path):
+    if type(test) is str:
+        create_file_jinja(path + "/" + test, test)
+
+
+
